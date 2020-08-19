e88e87a1d51ad444b73c6ea2da5f37189390d789
HP <efafafaef>
2020-08-19 13:24:20 +0300

    feat: add button for reset filter

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 let image = document.querySelector('img');
 let buttonList = document.querySelectorAll('button');
 
+function addFilter() {
+	currentFilter = this.dataset.value;
+	switch(currentFilter) {
+		case 'none':
+			image.style.filter = `${currentFilter}`;
+			break;
+		case 'contrast':
+			image.style.filter = `${currentFilter}(1.5)`;
+			break;
+		case 'saturate':
+			image.style.filter = `${currentFilter}(200%)`;
+			break;
+		case 'invert':
+		case 'sepia': 
+			image.style.filter = `${currentFilter}(1)`;
+			break;
+		case 'blur':
+			image.style.filter = `${currentFilter}(1.5px)`;
+			break;
+	}
+}
+
 buttonList.forEach(button => {
-	button.addEventListener('click', function() {
-		if(button.style) button.style = 'none';
-		currentFilter = button.dataset.value;
-		switch(currentFilter) {
-			case 'contrast':
-				image.style.filter = `${currentFilter}(1.5)`;
-				break;
-			case 'saturate':
-				image.style.filter = `${currentFilter}(200%)`;
-				break;
-			case 'invert':
-			case 'sepia': 
-				image.style.filter = `${currentFilter}(1)`;
-				break;
-			case 'blur':
-				image.style.filter = `${currentFilter}(1.5px)`;
-				break;
-		}
-	});
+	button.addEventListener('click', addFilter);
 });
